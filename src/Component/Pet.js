178437ae00d4bc0b47c34c7eca656cb3594881d4
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-30 11:03:34 -0400

    add click event but it's no hooked up

--- src/Component/Pet.js
+++ src/Component/Pet.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 class Pet extends Component {
+
+    savePetToLocalStorage = () => {
+        console.log("button was clicked")
+        
+    }
     render() {
         return (
             <section className="pet" >
               <header>{this.props.pet.name.$t}</header>
               <img src={this.props.pet.media.photos.photo[3].$t} />
-              <button>Save for Later! arf!</button>
+              <button onClick={this.savePetToLocalStorage}>Save for Later! arf!</button>
             </section>
         );
     }
